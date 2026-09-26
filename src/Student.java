a2cc7081601ad00dca5624cffccffaf6e0031cc6
Rashmivishmika1229 <116312974+Rashmivishmika1229@users.noreply.github.com>
2026-09-26 12:00:31 +0530

    Add Student constructor

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,11 @@ public class Student {
     private String name;
     private String programme;
     private double marks;
+
+    public Student(int studentId, String name, String programme, double marks) {
+        this.studentId = studentId;
+        this.name = name;
+        this.programme = programme;
+        this.marks = marks;
+    }
 }
