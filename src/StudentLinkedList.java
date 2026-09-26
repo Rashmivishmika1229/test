79f53cd48e9854618d52922f7458eac9fad3dd6f
Rashmivishmika1229 <116312974+Rashmivishmika1229@users.noreply.github.com>
2026-09-26 12:22:40 +0530

    Implement student search

--- src/StudentLinkedList.java
+++ src/StudentLinkedList.java
@@ -10,4 +10,15 @@ public class StudentLinkedList {
 
     private Node head;
     private int size;
+
+    public Student search(int studentId) {
+        Node current = head;
+        while (current != null) {
+            if (current.data.getStudentId() == studentId) {
+                return current.data;
+            }
+            current = current.next;
+        }
+        return null;
+    }
 }
